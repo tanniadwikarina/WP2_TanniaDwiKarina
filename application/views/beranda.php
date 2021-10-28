43112d73a21a55064dbdc579096b3b8b11307512
tanniadwikarina <karinaatannia01@gmail.com>
2021-10-28 19:08:02 +0700

    menambahkan tugas

---
+++ application/views/beranda.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman_beranda</title>
+</head>
+<body>
+    <h2>Hai nama saya adalah Tannia Dwi Karina </h2>
+</body>
+</html>
